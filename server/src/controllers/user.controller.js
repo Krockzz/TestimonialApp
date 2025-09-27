48db6f84a9c7053395996b1b5e4edb8c71123cbe
Krockzz <krunalhunge366@gmail.com>
2025-09-27 16:20:30 +0530

    Updated

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -195,7 +195,7 @@ const loginUser = asyncHandler(async(req, res) => {
 
        const options =  {
        httpOnly: true,
-  secure: false,
+       secure: false,
   sameSite: "Lax",
   path: "/",
 
