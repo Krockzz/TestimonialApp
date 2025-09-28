42992c64d0d0e51068ee9744f5061bbb55d5686e
Krockzz <krunalhunge366@gmail.com>
2025-09-28 12:43:26 +0530

    Updated

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -37,6 +37,7 @@ router.get(
         httpOnly: true,
         secure: true,         // Required for SameSite=None
         sameSite: "none",     // Allows cross-domain
+        path : "/",
         maxAge: 15 * 60 * 1000, // 15 minutes
       });
 
@@ -44,6 +45,7 @@ router.get(
         httpOnly: true,
         secure: true,
         sameSite: "none",
+        path : "/",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
