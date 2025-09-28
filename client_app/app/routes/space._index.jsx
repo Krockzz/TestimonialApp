6ec54ce149481461dcd2cd5e147e869a98df4acd
Krockzz <krunalhunge366@gmail.com>
2025-09-28 13:43:14 +0530

    Updated

--- client_app/app/routes/space._index.jsx
+++ client_app/app/routes/space._index.jsx
@@ -8,6 +8,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function loader({ request }) {
 const cookieHeader = request.headers.get("Cookie");
+console.log(cookieHeader)
 
   const response = await fetch(`${API_URL}/api/v1/users/spaces/getSpaces`, {
     method: "GET",
